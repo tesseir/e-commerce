3107ce458ec95ced7829b63a33d65d623efedd70
tesseir <tesseir.edu@gmail.com>
2023-02-20 22:26:26 -0600

    finished models, started on category routes

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,8 +10,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      isNumeric: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references:{
+        model: Category,
+        key: 'id',
+      }
+    }, 
   },
+
   {
     sequelize,
     timestamps: false,
@@ -18,6 +46,7 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
+  
 );
 
 module.exports = Product;
